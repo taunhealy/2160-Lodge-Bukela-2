8b60c441a9cfa8c92a4fcabcdd1c96504d908723
taunhealy <taunhealy@gmail.com>
2023-03-27 13:24:58 +0200

    Update CoverBlockComponent.ts

--- blocks/CoverBlock/CoverBlockComponent.ts
+++ blocks/CoverBlock/CoverBlockComponent.ts
@@ -1,32 +1,30 @@
-
 import Image from "next/image";
 import { Block } from 'payload/types';
 import RichText from "../../components/RichText";
 import { CoverBlockType } from "./CoverBlockFields";
 import React, { ReactElement } from 'react';
 
+export const getStaticProps = async () => {
+
+  const res = await fetch('http://localhost:3000/api/blocks');
+  const data = await res.json();
+
+  return {
+props: { blocks: data }
+  }
+}
 
 const CoverBlockComponent: React.FC<CoverBlockType> = (props) => {
   const { content, image } = props;
 
   return (
     <div className="relative w-full h-screen">
-      <Image
-        src={image.filename}
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        alt={image.alt}
-        className="z-0"
-      />
       <div className="absolute inset-0 flex items-center justify-center z-10">
         <div className="px-2">
           <div className="py-2">
             <RichText
               content={content}
-              classNames={{
-                h1: "text-6xl font-bold text-white",
-              }}
+              classname="text-4xl font-bold text-center text-white"
             />
           </div>
         </div>
